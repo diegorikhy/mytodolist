4560a93110c790510cffe2c58d8c760a2099d089
diegorikhy <gtidiegohvieira@gmail.com>
2019-11-04 13:54:01 -0300

    remove digest false from assets path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,6 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'templates')
 Rails.application.config.assets.paths << Rails.root.join('app', 'javascript')
-Rails.application.config.assets.digest = false
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
